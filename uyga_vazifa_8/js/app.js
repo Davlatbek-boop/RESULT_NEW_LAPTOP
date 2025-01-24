0a2bb3e98eb7e175aec68d75047aa2d760863c55
Davlatbek-boop <salimovdavlat0526@gmail.com>
2025-01-24 14:24:23 +0500

    uyga vazifa 8

---
+++ uyga_vazifa_8/js/app.js
@@ -0,0 +1,44 @@
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>masala-1
+// function otaOgilFarq(otaYoshi, ogilYoshi) {
+//     let farq = otaYoshi - ogilYoshi;
+//     return farq - ogilYoshi;
+// }
+// let otaYoshi = +prompt("Otaning yoshini kiriting");
+// let ogilYoshi = +prompt("O'g'ilning yoshini kiriting");
+// console.log(otaOgilFarq(otaYoshi, ogilYoshi));
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>masala-2
+// const findFactors = (son) =>{
+//     for (let i = 1; i<=son/2; i++){
+//         if (son % i == 0){
+//             console.log(i)
+//         }
+//     }
+//     console.log(son)
+// }
+// findFactors(+prompt("Son kiriting>>>"))
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>masala-3
+// const isPrime = son =>{
+//     for (let i = 2; i <= son/2; i++){
+//         if (son % i == 0){
+//             return console.log(`${son} Tub son emas!!!`);
+//         }
+//     }
+//     return console.log(`${son} Tub son!!!`);
+// }
+// isPrime(+prompt("Son kiriting"))
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>masala-3
+// const power = (son1, son2) => {
+//     let natija = 1;
+//     while (son2){
+//         natija *= son1;
+//         son2--;
+//     }
+//     return natija;
+// }
+// console.log(power(+prompt("birinchi sonni kiriting>>>"),+prompt("ikkinchi sonni kiriting>>>>")));
