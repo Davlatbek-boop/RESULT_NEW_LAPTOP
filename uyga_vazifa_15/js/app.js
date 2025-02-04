dd549c1a3e58cb42c7a5d6db47081532b946bca7
Davlatbek-boop <salimovdavlat0526@gmail.com>
2025-02-04 17:16:38 +0500

    uyga vazifa 15

---
+++ uyga_vazifa_15/js/app.js
@@ -0,0 +1,18 @@
+
+// function fetchCountry(davlat) {
+//   const country = fetch(`https://restcountries.com/v3.1/name/${davlat}`);
+//   country.then((response) => {
+//    return response.json()
+//   })
+//   .then((response) => {
+//    console.log(response)
+//   })
+//   .catch((error)=>{
+//    console.log(error);
+//   })
+//   .finally(()=>{
+//    console.log("Ish tugadi");
+//   })
+// }
+
+// fetchCountry("Russia")
