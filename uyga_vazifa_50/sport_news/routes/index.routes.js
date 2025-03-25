6f6417cd754b21ad5c6eabb64108f31a13cc5e2b
Davlatbek <salimovdavlat0526@.gmail.com>
2025-03-25 16:27:59 +0500

    uyga vazifa 50

---
+++ uyga_vazifa_50/sport_news/routes/index.routes.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const langsRoute = require("./langs.routes");
+const tagsRoute = require("./tags.routes");
+const categoryRoute = require("./category.routes");
+const newsWithLangsRoute = require("./newsWithLangs.routes");
+const usersRoute = require("./users.routes");
+const newsRoute = require("./news.routes");
+const mediaRoute = require("./media.routes");
+const commentsRoute = require("./comments.routes");
+const reportsRoute = require("./reports.routes");
+const likesRoute = require("./likes.routes");
+const viewsRoute = require("./views.routes");
+const newsTagsRoute = require("./newsTags.routes")
+const authorsRoute = require("./authors.routes")
+const notifactionRoute = require("./notifactions.routes")
+
+router.use("/langs", langsRoute);
+router.use("/tags", tagsRoute);
+router.use("/category", categoryRoute);
+router.use("/newswlangs", newsWithLangsRoute);
+router.use("/users", usersRoute);
+router.use("/news", newsRoute);
+router.use("/media", mediaRoute);
+router.use("/comments", commentsRoute);
+router.use("/reports", reportsRoute);
+router.use("/likes", likesRoute);
+router.use("/views", viewsRoute);
+router.use("/newstag", newsTagsRoute)
+router.use("/author", authorsRoute)
+router.use("/notifaction", notifactionRoute)
+
+module.exports = router;
